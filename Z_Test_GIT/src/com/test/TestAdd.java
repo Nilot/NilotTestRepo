2c1b64eb170cb475f7863e51655b2f45a35131b7
n.nayandas.baral <n.nayandas.baral@BDC6-LX-705125F.dir.svc.accenture.com>
2017-05-10 15:28:45 +0530

    first push

--- Z_Test_GIT/src/com/test/TestAdd.java
+++ Z_Test_GIT/src/com/test/TestAdd.java
@@ -2,6 +2,6 @@ package com.test;
 
 public class TestAdd {
  public static void main(String arg[]){
-	 System.out.println("Newly added.Again.1232");
+	 System.out.println("Newly added.Again.1232.Added first push");
  }
 }
