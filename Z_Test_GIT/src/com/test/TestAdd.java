bc4948ed997cf416ed01cc6002894fa229e8c1fb
n.nayandas.baral <n.nayandas.baral@BDC6-LX-705125F.dir.svc.accenture.com>
2017-05-10 15:32:24 +0530

    second pushed now

--- Z_Test_GIT/src/com/test/TestAdd.java
+++ Z_Test_GIT/src/com/test/TestAdd.java
@@ -2,6 +2,6 @@ package com.test;
 
 public class TestAdd {
  public static void main(String arg[]){
-	 System.out.println("Newly added.Again.1232.Added first push");
+	 System.out.println("Newly added.Again.1232.Added first push, Given second push");
  }
 }
