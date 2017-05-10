9020e6ba82cb327cb20adf4de4e5f6d71e94ee62
n.nayandas.baral <n.nayandas.baral@BDC6-LX-705125F.dir.svc.accenture.com>
2017-05-10 14:52:06 +0530

    test change

--- Z_Test_GIT/src/com/test/TestAdd.java
+++ Z_Test_GIT/src/com/test/TestAdd.java
@@ -2,6 +2,6 @@ package com.test;
 
 public class TestAdd {
  public static void main(String arg[]){
-	 System.out.println("Newly added.Again.123");
+	 System.out.println("Newly added.Again.1232");
  }
 }
